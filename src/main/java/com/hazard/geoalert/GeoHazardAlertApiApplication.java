39017a25eefc95fc8c15693a6e07f16b36219311
Cristian Mitchell Harders López <cristianharders@gmail.com>
2025-11-24 19:44:11 +0100

    first commit

---
+++ src/main/java/com/hazard/geoalert/GeoHazardAlertApiApplication.java
@@ -0,0 +1,13 @@
+package com.hazard.geoalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoHazardAlertApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoHazardAlertApiApplication.class, args);
+	}
+
+}
