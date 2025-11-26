441d81f5a590b2a3f690a0fee79c1354f9666418
Cristian Mitchell Harders López <cristianharders@gmail.com>
2025-11-26 11:50:34 +0100

    Added service to get USGS api

---
+++ src/main/java/com/hazard/geoalert/service/USGSEarthquakeService.java
@@ -0,0 +1,52 @@
+package com.hazard.geoalert.service;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.hazard.geoalert.adapter.earthquake.EarthquakeAdapter;
+import com.hazard.geoalert.dto.usgs.USGSFeature;
+import com.hazard.geoalert.dto.usgs.USGSResponse;
+import com.hazard.geoalert.model.Earthquake;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class USGSEarthquakeService {
+
+    @Autowired
+    RestTemplate restTemplate;
+    @Autowired
+    EarthquakeAdapter earthquakeAdapter;
+
+    public List<Earthquake>fetchEarthquakes(String startDate){
+
+        List<Earthquake> earthquakes = new ArrayList<>();
+
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("UTC"));
+        String isoEndDate = now.format(DateTimeFormatter.ISO_DATE_TIME);
+        LocalDateTime StartDate = now.minusHours(24);
+        String isoStartDate = StartDate.format(DateTimeFormatter.ISO_DATE_TIME);
+
+
+        String url =  "https://earthquake.usgs.gov/fdsnws/event/1/query?" + "format=geojson&starttime=" + isoStartDate + "&endtime=" + isoEndDate;
+        ResponseEntity<USGSResponse>response = restTemplate.getForEntity(url, USGSResponse.class);
+
+        USGSResponse USGSbody = response.getBody();
+
+        for (USGSFeature x :USGSbody.getFeatures()) {
+            Earthquake earthquake = earthquakeAdapter.transformAlert(x);
+            earthquakes.add(earthquake);
+        }
+
+        return earthquakes;
+
+
+
+    }
+
+
+}
